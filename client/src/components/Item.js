04b6dfdb8cb85860bba0a795d075350a7734c1b7
tanner gaucher <tannermichaelgaucher@gmail.com>
2018-08-07 19:18:23 +0700

    formatting

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,64 +1,60 @@
-import React from 'react';
-import axios from 'axios';
+import React from 'react'
+import axios from 'axios'
 
 class Item extends React.Component {
-  
-  constructor(){
+  constructor() {
     super()
-    
+
     this.state = {
-      edit: false,
+      edit: false
     }
   }
-  
+
   toggleEditForm = () => {
-    console.log("toggle")
+    console.log('toggle')
     this.setState({
       edit: !this.state.edit
     })
   }
-  
+
   handleClick = () => {
-    this.toggleEditForm();
+    this.toggleEditForm()
   }
-  
+
   handleChange = e => {
-    const updatedItem = { 
-      ...this.props.item, 
-      [e.currentTarget.name]: e.currentTarget.value};
-      
+    const updatedItem = {
+      ...this.props.item,
+      [e.currentTarget.name]: e.currentTarget.value
+    }
+
     this.props.updateItem(this.props.index, updatedItem)
   }
-  
+
   handleSaveChangesClick = () => {
-    this.props.updateItem(this.props.index, this.props.item);
-    this.toggleEditForm();
-    
+    this.props.updateItem(this.props.index, this.props.item)
+    this.toggleEditForm()
+
     console.log('handlesaveChanges')
-    
-                              //req.params.id 
+
+    //req.params.id
     axios.post(`/updateItem/${this.props.details.id}`, {
       name: this.props.details.name,
       price: this.props.details.price,
       rating: this.props.details.rating
     })
   }
-  
+
   handleDeleteClick = () => {
     this.props.deleteItem(this.props.index)
-    
-                              //req.params.id
+
+    //req.params.id
     axios.post(`/deleteItem/${this.props.details.id}`)
   }
-  
-   
-  render() {
-    
-    if(this.props.details) {
-      
-      return(
-        <div className="item">
 
+  render() {
+    if (this.props.details) {
+      return (
+        <div className="item">
           <p>Name: {this.props.details.name}</p>
           <p>Price: {this.props.details.price}</p>
           <p>Rating: {this.props.details.rating}</p>
@@ -67,13 +63,12 @@ class Item extends React.Component {
             <button onClick={this.handleClick}>Edit</button>
           </div>
 
-          <div className={this.state.edit === false ? "hide" : "show"}>
-            <input name="name" onChange={this.handleChange} value={this.props.details.name}/>
-            <input name="price" onChange={this.handleChange} value={this.props.details.price}/>
-            <input name="rating" onChange={this.handleChange} value={this.props.details.rating}/>
+          <div className={this.state.edit === false ? 'hide' : 'show'}>
+            <input name="name" onChange={this.handleChange} value={this.props.details.name} />
+            <input name="price" onChange={this.handleChange} value={this.props.details.price} />
+            <input name="rating" onChange={this.handleChange} value={this.props.details.rating} />
             <button onClick={this.handleSaveChangesClick}>Save Changes</button>
           </div>
-
         </div>
       )
     }
@@ -81,4 +76,4 @@ class Item extends React.Component {
   }
 }
 
-export default Item;
+export default Item
